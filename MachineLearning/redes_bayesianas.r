1f69a3a9a7f98644713deaedb775c86710266b2c
Guilherme Fernando Angelico <angelicogfa@gmail.com>
2018-12-23 20:43:22 -0200

    inclusão de algoritmo de redes bayesianas

--- MachineLearning/redes_bayesianas.r
+++ MachineLearning/redes_bayesianas.r
@@ -1,16 +1,16 @@
 install.packages("bnlearn");
-install.packages("caret");
+install.packages("caret", dependencies = TRUE);
 
 library(bnlearn);
 library(caret);
 
 # help(hc);
 #  hill-climbing
-res = hc(insurance)
-# plot(res);
+res = hc(insurance);
+plot(res);
 
-modelo = bn.fit(res, data = insurance)
-modelo$GoodStudent
+modelo = bn.fit(res, data = insurance);
+modelo$GoodStudent;
 
 cpquery(modelo, event = (Accident == "Moderate" | Accident == "Severe"),
-evidence = (Age == "Senior" & RiskAversion == "Adventurous" & MakeModel == "SporstCar"));
+evidence = (Age == "Senior" & RiskAversion == "Adventurous" & MakeModel == "SportsCar"));
